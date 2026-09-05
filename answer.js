b9e2a805a3b37042bd7462bdc47aaf4147a08108
Emon Ahmed <emon0860@gmail.com>
2026-09-05 22:19:18 +0600

    Dhaka CNG Fare Meter task complite

--- answer.js
+++ answer.js
@@ -1,3 +1,4 @@
+// Question No. 1
 function describeValue(val) {
   if (!!val) {
     return `${typeof val} | truthy`;
@@ -6,6 +7,7 @@ function describeValue(val) {
   }
 }
 
+// Question No. 2
 function getDayType(val) {
   switch (val) {
     case "Friday":
@@ -25,6 +27,7 @@ function getDayType(val) {
   }
 }
 
+// Question No. 3
 function validateUsername(val) {
   if (val.length < 4) {
     return "Too Short";
@@ -36,4 +39,27 @@ function validateUsername(val) {
     return "Available";
   }
 }
+
+// Question No. 4
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+  let distancecharj;
+  let waicharj = waitingMinutes * 2;
+  if (distance > 2) {
+    let extraDistance = distance - 2;
+    let extraDistanceCharj = extraDistance * 15;
+    distancecharj = 50 + extraDistanceCharj;
+  } else {
+    distancecharj = 50;
+  }
+  if (isNight) {
+    let nightCharj = ((distancecharj + waicharj) / 100) * 20;
+    totalFare = distancecharj + waicharj + nightCharj;
+    return totalFare;
+  } else {
+    totalFare = distancecharj + waicharj;
+    return totalFare;
+  }
+}
+
+// Question No. 5
 
