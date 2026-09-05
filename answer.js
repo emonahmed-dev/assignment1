87481bd207e29a0527db0e1b2f1e7c6bcfbe0dde
Emon Ahmed <emon0860@gmail.com>
2026-09-05 19:50:45 +0600

    Bangladesh Weekend Machine task complite

--- answer.js
+++ answer.js
@@ -1,9 +1,27 @@
 function describeValue(val) {
-    if(!!val){
-        return (`${typeof val} | truthy`)
-    } else {
-        return (`${typeof val} | falsy`)
-    }
+  if (!!val) {
+    return `${typeof val} | truthy`;
+  } else {
+    return `${typeof val} | falsy`;
+  }
 }
 
+function getDayType(val) {
+  switch (val) {
+    case "Friday":
+    case "Saturday":
+      return "Weekend";
+      break;
+    case "Sunday":
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+      return "Working Day";
+      break;
+    default:
+      return "Invalid Day";
+      break;
+  }
+}
 
